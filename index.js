d61838552de32adb5d93c348cb9e3cacabac1c55
Stone Sun <stonesun@objectivasoftware.com>
2018-02-28 15:59:12 +0800

    Init version

---
+++ index.js
@@ -0,0 +1,70 @@
+'use strict';
+
+var React = require('react');
+
+var PureComponent = React.PureComponent;
+var h = React.createElement;
+
+function _inherits(subClass, superClass) {
+    subClass.prototype = Object.create(superClass.prototype, {
+        constructor: {
+            value: subClass,
+            writable: true,
+            configurable: true
+        }
+    });
+
+    if (superClass) {
+        Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
+    }
+}
+
+function extend(obj, props) {
+    for (var i in props) {
+        obj[i] = props[i];
+    }
+}
+
+function wrap(Context, key, WrappedComp) {
+    function Connected(props) {
+        PureComponent.call(this, props);
+    }
+
+    _inherits(Connected, PureComponent);
+    extend(Connected.prototype, {
+        render: function render() {
+            var props = this.props;
+            return h(Context.Consumer, null, function (context) {
+                var tmpProps = {};
+                tmpProps[key] = context;
+                return h(WrappedComp, Object.assign(tmpProps, props));
+            });
+        }
+    });
+
+    Connected.WrappedComp = WrappedComp;
+    var wrappedCompName = WrappedComp.displayName || WrappedComp.name || "Component";
+    var consumerName = Context.Consumer.displayName || Context.Consumer.name || "Context.Consumer";
+    Connected.displayName = wrappedCompName + "(" + consumerName + "." + key + ")";
+
+    return Connected;
+};
+
+function withContext(Context) {
+    var key = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : "context";
+
+    return function (WrappedComp) {
+        return wrap(Context, key, WrappedComp);
+    };
+}
+
+function withMultiContext(map) {
+    return function (WrappedComp) {
+        return Object.keys(map).reduce(function (Comp, key) {
+            return wrap(map[key], key, Comp);
+        }, WrappedComp);
+    };
+}
+
+exports.withContext = withContext;
+exports.withMultiContext = withMultiContext;
